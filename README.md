9da4bdfde013d82fc0f5aa037c70c9dbc10cdf0b
Gehad Shaat <gehad.shaath@gmail.com>
2017-06-16 23:51:46 -0700

    How to use documentation

--- README.md
+++ README.md
@@ -3,5 +3,25 @@ Bandwidth BXML Generator for Python
 
 Pure python implmenetation of Bandwidth BXML generator that works without needing to install lxml. This library works on AWS Lambda without needing to compile lxml.
 
+
+# usage
+```python
+from bandwidth_bxml import Response
+
+def speak():
+    response = Response()
+    response.speak("Hello World")
+    return response.to_string()
+
+
+def gather():
+    response = Response()
+    gather = response.gather("https://gather.url/nextBXML")
+    gather.speak("Please, press a digit")
+    return response.to_string()
+    
+```
+
+    
 # Build instructions
 python setup.py build sdist
--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(
-    name='bandwidth_bxml',
-    version='0.1',
-    packages=[''],
-    url='https://github.com/wavycloud/bandwidth_bxml',
-    license='',
-    author='WavyCloud',
-    author_email='gehad.shaath@gmail.com',
-    description=''
-)
+setup(name='bandwidth_bxml',
+      version='0.3',
+      packages=find_packages(),
+      description='Bandwidth BXML Generator in pure python',
+      author='WavyCloud',
+      author_email='gehad.shaath@gmail.com',
+      url='https://github.com/wavycloud/bandwidth_bxml',
+      py_modules=['bandwidth_bxml'],
+      install_requires=[],
+      license='MIT License',
+      zip_safe=True,
+      keywords='bandwidth bxml',
+      classifiers=[])
